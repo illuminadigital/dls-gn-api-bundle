01c3545dedf3fe32add9003ea98f99a91118245d
Gary Hogarth <gary@garyhogarth.com>
2013-09-09 14:49:44 +0400

    config

--- Illumina/GnApiBundle/DependencyInjection/Configuration.php
+++ Illumina/GnApiBundle/DependencyInjection/Configuration.php
@@ -12,9 +12,6 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
@@ -22,24 +19,22 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-            ->scalarNode('base_url')
-            ->defaultNull()
-            ->info('The endpoint URL of the GN Site')
-            ->example('http://www.foo.bar/cas')
-            ->end()
-            ->scalarNode('key')
-            ->defaultNull()
-            ->info('The private key for the GN site')
-            ->example('WEarQRBHuiWtsnveoabnq304y039u5gw4')
-            ->end()
-            ->scalarNode('secret')
-            ->defaultNull()
-            ->info('The secret for the GnN site')
-            ->example('SRTbn9sdrtkseQRB6rght89tltnETjhtW')
-            ->end()
+                ->scalarNode('base_url')
+                    ->defaultNull()
+                    ->info('The endpoint URL of the GN Site')
+                    ->example('http://www.foo.bar/cas')
+                    ->end()
+                ->scalarNode('key')
+                    ->defaultNull()
+                    ->info('The private key for the GN site')
+                    ->example('WEarQRBHuiWtsnveoabnq304y039u5gw4')
+                    ->end()
+                ->scalarNode('secret')
+                    ->defaultNull()
+                    ->info('The secret for the GN site')
+                    ->example('SRTbn9sdrtkseQRB6rght89tltnETjhtW')
+                    ->end()
             ->end();
-
-        return $treeBuilder;
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
