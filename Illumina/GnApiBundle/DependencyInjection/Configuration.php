9eb20c272d056cd5728cad7dab35350835281992
Gary Hogarth <gary@garyhogarth.com>
2013-09-09 14:56:02 +0400

    config

--- Illumina/GnApiBundle/DependencyInjection/Configuration.php
+++ Illumina/GnApiBundle/DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('illumina_gn');
+        $rootNode = $treeBuilder->root('illumina_gn_api');
 
         $rootNode
             ->children()
